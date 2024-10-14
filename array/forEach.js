5a65875a6d2b1d532ab9e177535f5d285db32a98
PavelGoblin <pvlbrhm@gmail.com>
2024-10-14 16:03:35 +0600

    functiON & object dONe

---
+++ array/forEach.js
@@ -0,0 +1,10 @@
+//() =functions
+//{} = Objects
+//[]=Array
+
+
+var arr =[10,20,30,40,50,60]
+
+arr.forEach(function(elem){
+  console.log('hello',elem);
+})
---
+++ array/object.js
@@ -0,0 +1,22 @@
+var obj1={
+  model:"iphone",
+  price:70000,
+  color:'white'
+}
+var obj2={
+  model:"oneplus",
+  price:30,
+  color:'blue'
+}
+var obj3={
+  model:"samsung",
+  price:50000,
+  color:'black'
+}
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+console.log(obj3.model);
+console.log(obj2.price);
+console.log(obj1.color);
+
